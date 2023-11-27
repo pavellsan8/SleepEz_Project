c933c53f16310a55b36a1f994580b4a380b5e8fb
pavellsan8 <pavelryan146@gmail.com>
2023-11-27 14:58:28 +0700

    oke

--- src/index.jsx
+++ src/index.jsx
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from 'react-router-dom';
-import {createBrowserRouter, RouterProvider, Route} from 'react-router-dom';
 
 // const router = createBrowserRouter([
 //   {
