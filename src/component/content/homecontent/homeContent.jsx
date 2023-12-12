88846d193e917cd7915db0d1837c0452dc928ab0
pavellsan8 <pavelryan146@gmail.com>
2023-12-12 10:08:32 +0700

    kasi animasi

--- src/component/content/homecontent/homeContent.jsx
+++ src/component/content/homecontent/homeContent.jsx
@@ -1,23 +1,34 @@
 import './homeContent.css';
 import HomeImage from "../../../asset/homeImageIlustration.png";
 import { useEffect, useState } from 'react';
+import { useTypewriter, Cursor } from 'react-simple-typewriter';
 
 function HomeContent() {
 
     const sleepText = ['Asleep', 'Stress', 'Insomnia']
-    const [sleepDisorder, setSleepDisorder] = useState(0)
+    // const [sleepDisorder, setSleepDisorder] = useState(0)
 
-    useEffect(() => {
-        const intervalChanged = setInterval(() => {
-            setSleepDisorder((textIndex) => (textIndex + 1) % sleepText.length);
-        }, 4000);
-    }, []);
+    const [text] = useTypewriter({
+        words: sleepText, 
+        loop: {},
+        typeSpeed: 125,
+        deleteSpeed: 100
+    });
+
+    // useEffect(() => {
+    //     const intervalChanged = setInterval(() => {
+    //         setSleepDisorder((textIndex) => (textIndex + 1) % sleepText.length);
+    //     }, 4000);
+    // }, []);
 
     return(
         <div className="home-bg">
             <div className="home-content">
                 <div className="home-content-text">
-                    <h1 className="text-title">Having Difficulty Falling {sleepText[sleepDisorder]}?</h1>
+                    <h1 className="text-title">Having Difficulty Falling
+                        <span> {text}?</span>
+                        <span style={{color: 'white'}}><Cursor cursorStyle='|'/></span>
+                    </h1>
                     <p className="text-subtitle">Congratulations! You've found the ideal place to sleep and rejuvenate yourself. Glad to be to your ultimate heaven for peaceful evenings and delightful dreams!</p>
                     <button className="started-btn">Get Started</button>
                 </div>
