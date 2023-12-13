c915c3a3645b8e7d6c3b3b52cb0dedf2adca7798
pavellsan8 <pavelryan146@gmail.com>
2023-12-13 10:27:25 +0700

    tambah not found page

--- src/component/content/notfoundcontent/notfoundContent.jsx
+++ src/component/content/notfoundcontent/notfoundContent.jsx
@@ -6,15 +6,15 @@ function NotfoundContent() {
   return (
     <div className='notfound-div'>
         <div className='error-title'>
-            <span className='blue-text'>4</span>
-            <span className='darkblue-text'>0</span>
-            <span className='blue-text'>4</span>
+          <span className='blue-text'>4</span>
+          <span className='darkblue-text'>0</span>
+          <span className='blue-text'>4</span>
         </div>
         <div className='error-subtitle'>
-            <p className='subtitle-text'>Hmm looks like you need some sleep here.</p>
+          <p className='subtitle-text'>Hmm looks like you need some sleep here.</p>
         </div>
         <Link to="/home" style={{textDecoration:'none'}}>
-            <button>Back to Home</button>
+          <button className='back-home-btn'>Back to Home</button>
         </Link>
     </div>
   )
