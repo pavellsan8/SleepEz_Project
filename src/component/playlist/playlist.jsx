cb3e360d70bd3a7699658cfd251b5888c7f06006
William4312 <gunawanandreas02@gmail.com>
2023-12-14 20:13:45 +0700

    menambahkan update pada playlist

--- src/component/playlist/playlist.jsx
+++ src/component/playlist/playlist.jsx
@@ -1,54 +1,38 @@
-import "./playlist.css";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
-import React, { useEffect } from "react";
-import { reducerCases } from "../../utils/constant.jsx";
 import { useStateProvider } from "../../utils/StateSupports.jsx";
-import { useState } from "react";
-import { cliendId } from "../../spotify.jsx";
+import { reducerCases } from "../../utils/constant.jsx";
+import "./playlist.css";
 
 function Playlist() {
-  const [{ token, playlists }, dispatch] = useStateProvider();
-  const PLAYLIST_ENDPOINT =
-    "https://api.spotify.com/v1/playlists/3cEYpjA9oz9GiPac4AsH4n";
-  const [responseData, setResponse] = useState("no data");
-  const storedToken = localStorage.getItem("token");
+  const [{ token }, dispatch] = useStateProvider();
+  const PLAYLIST_ENDPOINT = "https://api.spotify.com/v1/playlists/3cEYpjA9oz9GiPac4AsH4n";
+  const [playlists, setPlaylists] = useState([]);
 
   useEffect(() => {
-    const getPlaylistData = async () => {
-      setResponse(
-        await axios
-          .get(PLAYLIST_ENDPOINT,{
+    if (token) {
+      const getPlaylistData = async () => {
+        try {
+          const response = await axios.get(PLAYLIST_ENDPOINT, {
             headers: {
-              Authorization: "Bearer " + storedToken,
+              Authorization: `Bearer ${token}`,
               Accept: "application/json",
-              "Content-Type": "application/x-www-form-urlencoded",
-              'Client-ID': cliendId,
+              "Content-Type": "application/x-www-form-urlencoded"
             },
-          })
-          .then(function (res) {
-            console.log(`reponse: ${res}`);
-          })
-          .catch(function (err) {
-            console.log(`error: ${err}`);
-          })
-      );
+          });
+          const playlistsData = response.data
+         
+          console.log(playlistsData);
+          setPlaylists([{ name: playlistsData.name }]);
+      
+        } catch (err) {
+          console.error(`Error fetching playlist data: ${err}`);
+        }
+      };
 
-      // console.log(`response : ${responseData}`);
-
-      // const { items } = responseData.data;
-      // console.log(`data item dari playlits: ${items}`);
-      // const playlists = items.map(({ name, id }) => {
-      //   return { name, id };
-      // });
-      //console.log("API Response:", response.data);
-      dispatch({ type: reducerCases.SET_PLAYLISTS, playlists });
-    };
-
-    if (storedToken) {
-      getPlaylistData(PLAYLIST_ENDPOINT, playlists);
-      console.log(token);
+      getPlaylistData();
     }
-  }, [token, dispatch, playlists, responseData, storedToken]);
+  }, [token, dispatch]);
 
   const changeCurrentPlaylist = (selectedPlaylistId) => {
     dispatch({ type: reducerCases.SET_PLAYLIST_ID, selectedPlaylistId });
@@ -57,17 +41,11 @@ function Playlist() {
   return (
     <div className="user-playlist">
       <ul>
-        {playlists.map(({ name, id }) => {
-          return (
-            <li
-              key={id}
-              style={{ color: "white" }}
-              onClick={() => changeCurrentPlaylist(id)}
-            >
-              {name}
-            </li>
-          );
-        })}
+        {playlists.map(({ name, id }) => (
+          <li key={id} style={{ color: "white" }} onClick={() => changeCurrentPlaylist(id)}>
+            {name}
+          </li>
+        ))}
       </ul>
     </div>
   );
