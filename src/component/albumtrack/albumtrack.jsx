f02203b4a9513b34a764946d69513af1d967209e
pavellsan8 <pavelryan146@gmail.com>
2023-12-14 22:22:28 +0700

    playlist and track

--- src/component/albumtrack/albumtrack.jsx
+++ src/component/albumtrack/albumtrack.jsx
@@ -6,7 +6,7 @@ import { reducerCases } from "../../utils/constant.jsx";
 
 function AlbumTrack() {
     const [{ token }, dispatch] = useStateProvider();
-    const TRACK_ENDPOINT = "https://api.spotify.com/v1/playlists/3wdAo1Yvzss32Eue9GSilt/tracks";
+    const TRACK_ENDPOINT = "https://api.spotify.com/v1/playlists/37i9dQZF1DX3ZeFHRhhi7Y/tracks";
     const [tracks, setTracks] = useState([]);
   
     useEffect(() => {
@@ -20,10 +20,13 @@ function AlbumTrack() {
                 "Content-Type": "application/x-www-form-urlencoded"
               },
             });
-            const tracksData = response.data.items.tracks
+            const tracksData =  response.data.items.map(item => ({
+              name: item.track.name,
+              image: item.track.album.images[0].url,
+            }));
            
             console.log(tracksData);
-            setTracks([{ name: tracksData.name }]);
+            setTracks(tracksData);
         
           } catch (err) {
             console.error(`Error fetching playlist data: ${err}`);
@@ -31,27 +34,25 @@ function AlbumTrack() {
         };
   
         getTrackData();
+
       }
     }, [token, dispatch]);
-  
-    const changeCurrentPlaylist = (selectedPlaylistId) => {
-      dispatch({ type: reducerCases.SET_PLAYLIST_ID, selectedPlaylistId });
-    };
   
     return (
       <div className="user-playlist-container pt-5">
         <ul className="user-playlist">
-          {tracks.map(({ name, id }) => (
-            <li 
-              className="all-playlists"
-              key={id} 
-              onClick={() => changeCurrentPlaylist(id)}>
-              {name}
-            </li>
-          ))}
+        {tracks.map(({ name, image }, index) => (
+          <li 
+          key={index} >
+            <img 
+            src={image} 
+            alt={name} 
+            style={{ width: '200px', height: '200px', marginRight: '10px'}} />
+            {name}
+          </li>
+        ))}
         </ul>
       </div>
     );
 }
-
 export default AlbumTrack
--- src/component/playlist/playlist.jsx
+++ src/component/playlist/playlist.jsx
@@ -6,25 +6,32 @@ import "./playlist.css";
 
 function Playlist() {
   const [{ token }, dispatch] = useStateProvider();
-  const PLAYLIST_ENDPOINT = "https://api.spotify.com/v1/playlists/3wdAo1Yvzss32Eue9GSilt";
+  const PLAYLIST_ENDPOINTS = [
+    "https://api.spotify.com/v1/playlists/3wdAo1Yvzss32Eue9GSilt",
+    "https://api.spotify.com/v1/playlists/37i9dQZF1DX3ZeFHRhhi7Y",
+    "https://api.spotify.com/v1/playlists/1CqMKEmvV1bQ7Gnnew8e0Y",
+  ];
   const [playlists, setPlaylists] = useState([]);
 
   useEffect(() => {
     if (token) {
       const getPlaylistData = async () => {
         try {
-          const response = await axios.get(PLAYLIST_ENDPOINT, {
-            headers: {
-              Authorization: `Bearer ${token}`,
-              Accept: "application/json",
-              "Content-Type": "application/x-www-form-urlencoded"
-            },
-          });
-          const playlistsData = response.data
-         
+          const playlistsData = await Promise.all(
+            PLAYLIST_ENDPOINTS.map(async (endpoint) => {
+              const response = await axios.get(endpoint, {
+                headers: {
+                  Authorization: `Bearer ${token}`,
+                  Accept: "application/json",
+                  "Content-Type": "application/x-www-form-urlencoded",
+                },
+              });
+              return { name: response.data.name, id: response.data.id };
+            })
+          );
+
           console.log(playlistsData);
-          setPlaylists([{ name: playlistsData.name }]);
-      
+          setPlaylists(playlistsData);
         } catch (err) {
           console.error(`Error fetching playlist data: ${err}`);
         }
@@ -45,7 +52,8 @@ function Playlist() {
           <li 
             className="all-playlists"
             key={id} 
-            onClick={() => changeCurrentPlaylist(id)}>
+            onClick={() => changeCurrentPlaylist(id)}
+          >
             {name}
           </li>
         ))}
